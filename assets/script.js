3c42538105c49b76b179faa34cc5af46283c5fdb
Alexb <acbird.ab@gmail.com>
2022-05-14 16:38:49 -0600

    page setup is done and figuring out the color changing element for the text areas

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+var rowEl = $(".row");
+
+var hourIndex = $(".hour").each(function (index) {});
+console.log(hourIndex);
+
+var inputIndex = $("textarea").each(function (index) {});
+console.log(inputIndex);
+var buttonIndex = $(".saveBtn").each(function (index) {});
+console.log(buttonIndex);
+
+$("#currentDay").append(
+  "Todays Date: " + moment().format("MMMM Do YYYY, h:mm:ss a")
+);
+
+currentTime = new Date().getHours();
+console.log(currentTime);
+
+ColorChange();
+
+function ColorChange() {
+  for (var i = 0; i < hourIndex.length; i++)
+    if (rowEl.children("div").eq(i).text < currentTime) {
+      $(rowEl.children("textarea").eq(i)).addClass("past");
+    } else if (rowEl.children("div").eq(i).text > currentTime) {
+      $(rowEl.children("textarea").eq(i)).addClass("future");
+    } else if (rowEl.children("div").eq(i).text === currentTime) {
+      $(rowEl.children("textarea").eq(i)).addClass("present");
+    }else
+}
